7a301a054cb0146f795ab3a6c2e551f5446e2991
Luke Massetti <lukemassetti@WestSide-Luke.local>
2018-04-20 11:21:23 -0700

    updated js files

--- bam1.js
+++ bam1.js
@@ -1,9 +1,8 @@
-
 var mysql = require('mysql');
 var prompt = require('prompt');
-
 var inputString = process.argv;
 var rquested = 0;
+var globaltotal = 0;
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -11,100 +10,173 @@ var connection = mysql.createConnection({
     database: 'bam1_db'
 
 });
-
+var connection2 = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'bam1_db'
+})
 choice = process.argv[2];
 search_item = process.argv[3];
 quantity = process.argv[4];
-thechoice();
-function thechoice() {
-
-    if (choice === 'view') {
+salepoint();
+function salepoint() {
+    if (choice === 'view.all') {
         connection.connect((error) => {
             if (error) throw error
             console.log('connected as ', connection.threadId)
-            var sqlquery = "select * from products"
+            var sqlquery = "select * from product"
             connection.query(sqlquery, (error, response) => {
                 if (error) throw error
                 console.log("NAME" + "\t" + "\t" + "PRICE" + "\t" + "\t" + "QUANTITY")
                 for (var key in response) {
                     console.log(response[key].product_name + "\t" + "\t" + response[key].price + " $" + "\t" + "\t" + response[key].stock_quantity);
                 }
-
             })
             connection.end()
+        })
+    }
+    if (choice === 'order') {
+        var availableq = 0;
+        var unitprice = 0;
+        prompt.start();
+        prompt.get(['ItemName', 'Quantity'], function (err, result) {
+            if (err) { return onErr(err); }
+            var frmuseritm = result.ItemName
+            var frmuserqu = parseInt(result.Quantity)
+            connection.connect((error) => {
+                if (error) throw error
+                // console.log('connected as ', connection.threadId)
+                var sqlquery = "select * from product where product_name = " + "'" + frmuseritm + "'";
+                connection.query(sqlquery, (error, response) => {
+                    if (error) throw error
+                    availableq = response[0].stock_quantity;
+                    unitprice = response[0].price;
+                    // console.log(availableq,unitprice)
+                    if (frmuserqu <= availableq) {
+                        var orderquery = " update product set stock_quantity =" + (availableq - frmuserqu) + " where product_name = '" + frmuseritm + "';"
+                        connection.query(orderquery, (error, response) => {
+                            if (error) throw error
+                            console.log("Your Order Submeted")
+                            console.log("The Total Price is: " + unitprice * frmuserqu + " $")
+                        })
+                        connection.end()
+                    } else {
+                        console.log("The available stock is :" + availableq)
+                        console.log("The available stock is not enough for your order")
+                        connection.end()
+                    }
+                })
+            })
         })
     }
     if (choice === 'search') {
-        connection.connect((error) => {
-            if (error) throw error
-            console.log('connected as ', connection.threadId)
-            var sqlquery = "select * from products where product_name = " + "'" + search_item + "'";
-            connection.query(sqlquery, (error, response) => {
+        var availableq = 0;
+        var unitprice = 0;
+        prompt.start();
+        prompt.get(['ItemName'], function (err, result) {
+            if (err) { return onErr(err); }
+            var frmuseritm = result.ItemName
+            connection.connect((error) => {
                 if (error) throw error
-                //  console.log(response[0].price)
-                console.log("NAME" + "\t" + "\t" + "PRICE" + "\t" + "\t" + "QUANTITY")
-                for (var key in response) {
-                    console.log(response[key].product_name + "\t" + "\t" + response[key].price + " $" + "\t" + "\t" + response[key].stock_quantity);
-                }
-
-            })
-            connection.end()
-        })
-    }
-
-    if (choice === 'order') {
-
-        connection.connect((error) => {
-            if (error) throw error
-            console.log('connected as ', connection.threadId)
-            var sqlquery = "select * from products where product_name = " + "'" + search_item + "'";
-            connection.query(sqlquery, (error, response) => {
-                if (error) throw error
-                var selectedquantity = response[0].stock_quantity;
-                var unitprice = response[0].price;
-
-                prompt.start();
-                prompt.get(['Quantity'], function (err, result) {
-                    if (err) { return onErr(err); }
-
-                    var frmuserqu = parseInt(result.Quantity)
-
-
-
-                    if (frmuserqu <= selectedquantity) {
-
-                        var orderquery = " update products set stock_quantity =" + (selectedquantity - frmuserqu) + " where product_name = '" + search_item + "';"
-                        connection.connect((error) => {
-                            connection.query(orderquery, (error, response) => {
-                                if (error) throw error
-
-                                console.log("The total price is :"+ (frmuserqu*unitprice) + " $")
-                                console.log("your order submeted success")
-
-
-                            })
-                            connection.end()
-                        })
-
-                    } else {
-                        console.log("the quantitytaht available is :" + selectedquantity)
-                        connection.end()
+                //  console.log('connected as ', connection.threadId)
+                var sqlquery = "select * from product where product_name = " + "'" + frmuseritm + "'";
+                connection.query(sqlquery, (error, response) => {
+                    if (error) throw error
+                    //  console.log(response[0].price)
+                    console.log("NAME" + "\t" + "\t" + "PRICE" + "\t" + "\t" + "QUANTITY")
+                    for (var key in response) {
+                        console.log(response[key].product_name + "\t" + "\t" + response[key].price + " $" + "\t" + "\t" + response[key].stock_quantity);
                     }
-
-
-                });
-
-
-
-
+                })
+                connection.end()
             })
-
-
         })
     }
+    if (choice === 'manage') {
+        var sqlquery1 = "";
+        prompt.start();
+        console.log("please enter your choice :")
+        prompt.get(['choice'], function (err, result) {
+            var managchioce = result.choice;
+            if (managchioce === 'update') {
+                prompt.start();
+                prompt.get(['ItemName', 'Quantity'], function (err, result) {
+                    doit(2, result.ItemName, result.Quantity)
+                })
+            }
+            if (managchioce === 'low') {
+                sqlquery1 = "select * from product where stock_quantity <= 5"
+                doit(1, sqlquery1, 0)
+            }
+            if (managchioce === 'high') {
+                sqlquery1 = "select * from product where stock_quantity >= 50"
+                doit(1, sqlquery1, 0)
+            }
+            if (managchioce === 'new') {
+                prompt.start();
+                console.log("enter new product :")
+                prompt.get(['Name', 'price', 'quantity', 'department_name'], function (err, result) {
+                    sqlquery1 = "INSERT INTO product (product_name,price,stock_quantity,department_name) VALUES ('" + result.Name + "'," + result.price + " ," + result.quantity + " ,'" + result.department_name + "');"
+                    doit(3, sqlquery1, 0)
+                })
+            }
+        })
+    }
+    function datasetter(mydata) {
+        connection2.connect((error) => {
+            if (error) throw error
+            connection2.query(mydata, (error, response) => {
+                if (error) throw error
+                console.log("the product Quantity Changed")
+            })
+            connection2.end()
+        })
+    }
+    function doit(choice1, querty0, added) {
+        if (choice1 === 1) {
+            connection.connect((error) => {
+                if (error) throw error
+                //  console.log('connected as ', connection.threadId)
+                connection.query(querty0, (error, response) => {
+                    if (error) throw error
+                    console.log("NAME" + "\t" + "\t" + "PRICE" + "\t" + "\t" + "QUANTITY")
+                    for (var key in response) {
+                        console.log(response[key].product_name + "\t" + "\t" + response[key].price + " $" + "\t" + "\t" + response[key].stock_quantity);
+                    }
+                })
+                connection.end()
+            })
+        }
+        if (choice1 === 2) {
+            var currentq = 0;
+            var total = 0;
+            var orderquery = "iii";
+            connection.connect((error) => {
+                if (error) throw error
+                connection.query("select stock_quantity from product where product_name ='" + querty0 + "'", (error, response) => {
+                    if (error) throw error
+                    currentq = parseInt(response[0].stock_quantity)
+                    // console.log(currentq)
+                    var added2 = parseInt(added);
+                    total = currentq + added2;
+                    //  console.log(total)
+                    orderquery = " update product set stock_quantity =" + total + " where product_name = '" + querty0 + "';"
+                    datasetter(orderquery)
+                    connection.end()
+                })
+            })
+        }
+        if (choice1 === 3) {
+          // console.log(querty0)
 
-
-
-
-
+            connection2.connect((error) => {
+                if (error) throw error
+                connection2.query(querty0, (error, response) => {
+                    if (error) throw error
+                })
+                connection2.end()
+            })
+        }
+    }
 }
---
+++ bam2.sql
@@ -0,0 +1,18 @@
+-- drop database if EXISTS animals_db;
+-- create database animals_db ;
+-- use animals_db;
+-- create table people (
+--
+-- id int(11) auto_increment not null,
+-- name varchar (30) not NULL,
+-- has_pet boolean not NULL,
+-- pet_name varchar (30),
+-- pet_age int(10),
+-- primary key (id)
+-- );
+--
+--
+-- insert into people (name , has_pet,pet_name , pet_age ) VALUES ( 'jacob', true , 'misty' , 10);
+-- insert into people (name , has_pet,pet_name , pet_age ) VALUES ( 'peter', true ,'zoe'  , 2);
+--
+-- select * from people ;
